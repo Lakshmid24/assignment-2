b887d4345c06e827e6b6f888fb8fa54bbb95547e
Lakshmid24 <100596113+Lakshmid24@users.noreply.github.com>
2022-03-24 15:35:14 +0530

    Add files via upload
    assignment 2 program 1

---
+++ assignment 2 program 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#WAP tO get a list sorted in increasing order  by the last element in each tuple
+list1= [(2,5),(1,2),(4,4),(2,3),(2,1)]
+for i in range(len(list1)):
+    for j in range((len(list1))-i-1):
+        if list1[j][1]>list1[j+1][1]:
+            t=list1[j]
+            list1[j]=list1[j+1]
+            list1[j+1]=t
+print(list1)
+
+
+# In[ ]:
+
+
+
+
